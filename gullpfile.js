bae74d17da87c18f771c77c4be6117c3f34fb704
Yeiber Cano <yeibercano@gmail.com>
2016-06-16 13:56:17 -0700

    add stylelint to gulp tasks

--- gullpfile.js
+++ gullpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
 gulp.task('analyze-css', function () {
   var postcss = require('gulp-postcss');
+  var stylelint = require('stylelint');
   var reporter = require('postcss-reporter');
 
   return gulp.src('style.css')
     .pipe(postcss([ 
+      stylelint(),
       reporter()
     ]));
 });
