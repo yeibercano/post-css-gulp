80bde2edf2b7e96862070069491fb9d80f654016
Yeiber Cano <yeibercano@gmail.com>
2016-06-16 14:24:26 -0700

    testing grid and doiuse

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,20 @@ gulp.task('analyze-css', function () {
   var postcss = require('gulp-postcss');
   var stylelint = require('stylelint');
   var reporter = require('postcss-reporter');
+  var doiuse = require('doiuse');
 
   return gulp.src('style.css')
     .pipe(postcss([
+      doiuse({
+        browsers: ['ie >= 9', 'last 2 versions'],
+      }),
       stylelint(), 
       reporter()
     ]));
 });
+
+
+// return gulp.src('style.css')
+//   .pipe(postcss([
+//     reporter()
+// ]));
