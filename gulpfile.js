0378ca94fbecab1d70ee42e8390b8a035444d159
Yeiber Cano <yeibercano@gmail.com>
2016-06-16 15:51:07 -0700

    add more plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,40 @@
+
 var gulp = require('gulp');
 gulp.task('analyze-css', function () {
-  var postcss = require('gulp-postcss');
-  var stylelint = require('stylelint');
-  var reporter = require('postcss-reporter');
-  var doiuse = require('doiuse');
+    var postcss = require('gulp-postcss');
+    var doiuse = require('doiuse');
+    var immutableCss = require('immutable-css');
+    var stylelint = require('stylelint');
+    var reporter = require('postcss-reporter');
+    var cssstats = require('postcss-cssstats');
+    var sourcemaps   = require('gulp-sourcemaps');
+    var autoprefixer = require('autoprefixer');
+    var cssnext = require('postcss-cssnext');
+    var shortcss = require('postcss-short');
 
-  return gulp.src('style.css')
+    return gulp.src('style.css')
+    .pipe(sourcemaps.init())
     .pipe(postcss([
       doiuse({
         browsers: ['ie >= 9', 'last 2 versions'],
       }),
-      stylelint(), 
-      reporter()
-    ]));
+      shortcss,
+      cssnext,
+      autoprefixer({ 
+        browsers: ['last 5 versions'] }),
+      immutableCss({
+        strict: true
+      }),
+      stylelint(),
+      reporter(),
+      cssstats(
+        function(stats) {
+          console.log(stats);
+        }
+      )
+    ]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./dest'));
 });
 
-
-// return gulp.src('style.css')
-//   .pipe(postcss([
-//     reporter()
-// ]));
+        
